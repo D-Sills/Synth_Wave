95f4bfd02ebcb7a1f1b568be3fd047b584c7014d
Darren Sills <darrsills@gmail.com>
2023-12-06 19:20:07 +0000

    Rhythm mechanics working, BPM & onBeat()
    https://old.reddit.com/r/gamedev/comments/2fxvk4/heres_a_quick_and_dirty_guide_i_just_wrote_how_to/

--- Mobile game/scripts/modules/audio_manager.lua
+++ Mobile game/scripts/modules/audio_manager.lua
@@ -1,7 +1,11 @@
 local self = {}
 
-local now_playing = nil
-local bpm = 120
+NOW_PLAYING = nil
+BPM = 120
+CROCHET = 60 / BPM
+OFFSET = 0.0
+SONG_POSITION = 0.0 + OFFSET
+local next_beat_time = 0
 
 function self.play_music(event_name, fade_time)
     if not fade_time then fade_time = 0.5 end
@@ -9,19 +13,21 @@ function self.play_music(event_name, fade_time)
     local event_description = fmod.studio.system:get_event(event_name)
     local event = event_description:create_instance()
 
-    if now_playing == event then
+    if NOW_PLAYING == event then
         return
-    elseif now_playing then
-        self.fade_music(now_playing, 1.0, 0.0, fade_time) -- Fade out over 2 seconds
+    elseif NOW_PLAYING then
+        self.fade_music(NOW_PLAYING, 1.0, 0.0, fade_time) -- Fade out over 2 seconds
 
         timer.delay(fade_time, false, function()
-        now_playing = event
-        now_playing:start()
-        self.fade_music(now_playing, 0.0, 1.0, fade_time) -- Fade in over 2 seconds
+        NOW_PLAYING = event
+        NOW_PLAYING:start()
+        BPM = event:get_parameter_by_name("BPM")
+        self.fade_music(NOW_PLAYING, 0.0, 1.0, fade_time) -- Fade in over 2 seconds
     end)
     else -- No music playing
         event:start()
-        now_playing = event
+        NOW_PLAYING = event
+        BPM = event:get_parameter_by_name("BPM")
     end
 end
 
@@ -40,6 +46,31 @@ function self.play_sound_effect(event_name)
     local event_description = fmod.studio.system:get_event(event_name)
     local event = event_description:create_instance()
     event:start()
+end
+
+function self.update(dt)
+    if NOW_PLAYING then
+        local position = NOW_PLAYING:get_timeline_position()
+        SONG_POSITION = position / 1000 + OFFSET  -- Convert from milliseconds to seconds
+
+        -- Calculate the time for the next beat
+        CROCHET = 60 / BPM
+
+        -- Check if the current song position has reached the next beat time
+        if SONG_POSITION >= next_beat_time then
+            -- Trigger beat action
+            self.on_beat()
+
+            -- Calculate the time for the next beat
+            next_beat_time = next_beat_time + CROCHET
+        end
+    end
+end
+
+function self.on_beat()
+    -- Logic to execute on each beat
+    print("SONG_POSITION: " .. SONG_POSITION .. " On Beat!")
+    return true
 end
 
 return self
