088cbc43db6d0e8bd233142d2a6f5a6137306451
Darren Sills <darrsills@gmail.com>
2023-12-23 23:34:35 +0000

    code cleanup, results screen

--- Mobile game/scripts/modules/audio_manager.lua
+++ Mobile game/scripts/modules/audio_manager.lua
@@ -18,9 +18,15 @@ local next_beat_time = 0
 local last_beat_time = 0
 local last_position = 0
 
+local master_group
+
+function self.init()
+    master_group = fmod.studio.system:get_bus("bus:/")
+end
+
 function self.play_music(event_name, fade_time)
     if not fade_time then fade_time = 0.5 end
-    
+ 
     local event_description = fmod.studio.system:get_event("event:/" .. event_name)
     SONG_LENGTH = event_description:get_length()
     local event = event_description:create_instance()
@@ -29,12 +35,12 @@ function self.play_music(event_name, fade_time)
         if event_description == NOW_PLAYING:get_description()  then
             return
         end
-        self.fade_music(NOW_PLAYING, 1.0, 0.0, fade_time) -- Fade out over 2 seconds
+        self.fade_music(NOW_PLAYING, 1.0, 0.0, fade_time) -- fade out over 2 seconds
 
         timer.delay(fade_time, false, function()
         NOW_PLAYING = event
         NOW_PLAYING:start()
-        self.fade_music(NOW_PLAYING, 0.0, 1.0, fade_time) -- Fade in over 2 seconds
+        self.fade_music(NOW_PLAYING, 0.0, 1.0, fade_time) -- fade in over 2 seconds
     end)
     else -- No music playing
         event:start()
@@ -49,17 +55,17 @@ function self.stop_music(fade_time)
     if not fade_time then fade_time = 0.5 end
 
     if NOW_PLAYING then
-        self.fade_music(NOW_PLAYING, 1.0, 0.0, fade_time) -- Fade out over 2 seconds
+        self.fade_music(NOW_PLAYING, 1.0, 0.0, fade_time) -- fade out over 2 seconds
 
         timer.delay(fade_time, false, function()
-            --NOW_PLAYING:stop(fmod.STUDIO_STOP_MODE_ALLOWFADEOUT)
+            --NOW_PLAYING:stop(fmod.STUDIO_STOP_MODE_ALLOWFADEOUT) -- doesn't work in defold ig? this works in unity
             NOW_PLAYING = nil
         end)
     end
 end
 
 function self.fade_music(event_instance, start_volume, end_volume, duration)
-    local step = (end_volume - start_volume) / (duration * 60) -- Assuming 60 updates per second
+    local step = (end_volume - start_volume) / (duration * 60) -- should change depending on fps
     local current_volume = start_volume
     for i = 1, duration * 60 do
         timer.delay(i / 60, false, function()
@@ -79,25 +85,24 @@ function self.update(dt)
     if NOW_PLAYING then
         local position = NOW_PLAYING:get_timeline_position()
 
-        -- Check if the song has looped
+        -- check if the song has looped
         if position < last_position then
-            -- Song has looped; reset beat times
+            -- song has looped; reset beat times, not perfect but good enough
             next_beat_time = CROTCHET
             last_beat_time = 0
         end
         last_position = position
 
-        SONG_POSITION = position / 1000 + OFFSET  -- Convert from milliseconds to seconds
+        SONG_POSITION = position / 1000 + OFFSET  -- convert from milliseconds to seconds
 
-        -- Calculate the time for the next beat
+        -- next beat
         CROTCHET = 60 / BPM
 
-        -- Check if the current song position has reached the next beat time
+        -- check if the current song position has reached the next beat time
         if SONG_POSITION >= next_beat_time then
-            -- Trigger beat action
             self.on_beat()
 
-            -- Calculate the time for the next beat
+            -- calculate the time for the next beat
             last_beat_time = next_beat_time
             next_beat_time = next_beat_time + CROTCHET
         end
@@ -109,35 +114,6 @@ function self.on_beat()
     -- print("SONG_POSITION: " .. SONG_POSITION .. " On Beat!")
     for i, v in ipairs(BEAT_SUBSCRIBERS) do
         msg.post(v, "beat")
-    end
-end
-
--- Player action timing functions
-function self.handle_player_action()
-    local player_action_time = SONG_POSITION
-    local time_diff = self.check_beat_accuracy(player_action_time)
-    local rating = self.rate_player_action(time_diff)
-
-    -- Do something with the rating, like updating score or UI
-    print("Action Rating: " .. rating)
-    return rating
-end
-
-function self.check_beat_accuracy(player_action_time)
-    local time_to_prev_beat = player_action_time - last_beat_time
-    local time_to_next_beat = next_beat_time - player_action_time
-
-    local time_diff = math.min(math.abs(time_to_prev_beat), math.abs(time_to_next_beat))
-    return time_diff
-end
-
-function self.rate_player_action(time_diff)
-    if time_diff <= PERFECT_THRESHOLD then
-        return "Perfect"
-    elseif time_diff <= GOOD_THRESHOLD then
-        return "Good"
-    else
-        return "Bad"
     end
 end
 
@@ -169,6 +145,36 @@ end
 function self.set_bpm(new_bpm)
     BPM = new_bpm
     CROTCHET = 60 / BPM
+end
+
+function self.set_volume(volume)
+    master_group:set_volume(volume)
+end
+
+-- Player action timing functions
+function self.handle_player_action()
+    local player_action_time = SONG_POSITION
+    local time_diff = self.check_beat_accuracy(player_action_time)
+    local rating = self.rate_player_action(time_diff)
+
+    --print("Action Rating: " .. rating)
+    return rating
+end
+function self.check_beat_accuracy(player_action_time)
+    local time_to_prev_beat = player_action_time - last_beat_time
+    local time_to_next_beat = next_beat_time - player_action_time
+
+    local time_diff = math.min(math.abs(time_to_prev_beat), math.abs(time_to_next_beat))
+    return time_diff
+end
+function self.rate_player_action(time_diff)
+    if time_diff <= PERFECT_THRESHOLD then
+        return "Perfect"
+    elseif time_diff <= GOOD_THRESHOLD then
+        return "Good"
+    else
+        return "Bad"
+    end
 end
 
 return self
--- Mobile game/scripts/modules/utils.lua
+++ Mobile game/scripts/modules/utils.lua
@@ -3,12 +3,22 @@ local self = {}
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
-function self.lerp(a, b, t) -- this actually exists in the engine, but i missed it somehow
+DIFFCULTY = {
+    EASY = 0.4,
+    MEDIUM = 0.75,
+    HARD = 1
+}
+
+CHOSEN_DIFFICULTY = DIFFCULTY.MEDIUM
+
+function self.lerp(a, b, t) -- defold lerp is vector3 only
 	return a + (b - a) * t
 end
 
-function self.clamp(x,y,z)
-	return math.max(y, math.min(z,x))
+function self.clamp(val, lower, upper)
+    assert(val and lower and upper, "not very valid input")
+    if lower > upper then lower, upper = upper, lower end
+    return math.max(lower, math.min(upper, val))
 end
 
 function self.random_position()
@@ -27,6 +37,14 @@ end
 
 function self.get_screen_size()
 	return {width = SCREEN_WIDTH, height = SCREEN_HEIGHT}
+end
+
+function self.set_difficulty(difficulty)
+    CHOSEN_DIFFICULTY = difficulty
+end
+
+function self.get_difficulty()
+    return CHOSEN_DIFFICULTY
 end
 
 return self
